f7faee229b71e3fbf7d28cc13acde44ce06579cd
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-01-13 00:36:18 +0000

    data cleaning: updated variables names mostly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- package-prep/datasets/makeRData.R
+++ package-prep/datasets/makeRData.R
@@ -1,13 +1,15 @@
 
+dataDir <- "data"
+csvDir <- "csv"
+manDir <- "man"
+setwd(csvDir)
 csv.files <- dir(".", pattern = ".csv$")
-
-dataDir <- file.path("..","pkg","mosaicData","data")
-dataDir <- file.path('.')
+setwd('..')
 for (file in csv.files) {
 	name <- strsplit(file, '\\.')[[1]][1]
-	data <- read.csv(file)
+	data <- read.csv(file.path(csvDir,file))
 	assign(name, data)
 	objlist <- ls(pattern=name)
-	save(list=objlist, file=paste("data/", name, '.rda',sep=""))
-	prompt(name=name, file=paste("man/", name, '.Rd',sep=""))
+	save(list=objlist, file=file.path(dataDir, paste(name, '.rda',sep="")))
+	prompt(name=name, file=file.path(manDir, paste(name, '.Rd',sep="")))
 	}
