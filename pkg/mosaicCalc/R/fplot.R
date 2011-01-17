8bf7c8f170cf803cfe519857fdcf2b7eab653e8d
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-01-17 19:58:53 +0000

    updated fplot to allow for a list of functions as input and more flexibility
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaicCalc/R/fplot.R
+++ pkg/mosaicCalc/R/fplot.R
@@ -2,27 +2,64 @@
 fplot <- function(f, xlim, ylim, n=100, args=list(), type='l', xlab, ylab, 
 	... ) 
 {
-	if (! is.function(f) ) { 
-		stop('f must be a function') 
+	makeFunction <- function(f) {
+		if ( is.function(f) ) {
+			return(f)
+		}
+		if ( is.numeric(f) ) {
+			return ( function(x) { f } ) 
+		} 
+		if (is.character(f)) {
+			return ( function(x) { do.call( f, args=list(x) ) } )
+		}
+		stop( "Unable convert to a function." )
+	}
+
+	fList <- f
+	if (is.function(f) ) { 
+	 	fList = list(f)
+	}
+
+	if (missing(ylab)) {
+		if (length(fList) == 1) {
+			ylab=as.character(substitute(fList[[1]]))
+		} else {
+			ylab="function value"
+		}
+		ylab = as.character(f[[1]])
+	}
+
+	fList <- lapply(fList, makeFunction) 
+
+	if ( !all( unlist( lapply(fList, is.function) ) ) ) {
+		stop('f must be a function or list of functions') 
 	}
 	if (missing(xlab)) {
-		xlab=names(formals(f))[1]
-	}
-	if (missing(ylab)) {
-		ylab=as.character(substitute(f))
+		xlab=names(formals(fList[[1]]))[1]
 	}
 
 	if (missing(xlim)) {
-		if ( is.finite( do.call(f,args=c(list(0),args)) ) ) {
+		if ( is.finite( do.call(fList[[1]],args=c(list(0),args)) ) ) {
 			xlim <- c(-2,2)
 		} else {
 			xlim <- c(0,2)
 		}
 	}
+	ddd <- data.frame(x=numeric(0), y=numeric(0), group=character(0))
 
-	x <- adapt_seq(xlim[1], xlim[2], f=f, args=args, length.out=n)
-	y <- do.call(f, args=c(list(x), args))
-	xyplot(y ~ x, 
+	id <- 0
+	for (f in fList) {
+		id <- id+1
+		x <- adapt_seq(xlim[1], xlim[2], f=f, args=args, length.out=n)
+		x <- unique(x)
+		y <- do.call(f, args=c(list(x), args))
+		ddd <- rbind(ddd, 
+				data.frame(x=x, y=y, group= rep(as.character(id), length(x)))
+				)
+	}
+
+	xyplot(y ~ x , ddd,
+		groups = group, 
 		type=type, 
 		ylim=ylim, 
 		xlab=xlab, ylab=ylab, 
