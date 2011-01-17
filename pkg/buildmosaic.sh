f37113f203b14969e6c86553bfd2bf373d997965
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-01-17 22:20:53 +0000

    deleted bad reference to fastR [oops ;-)]
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/buildmosaic.sh
+++ pkg/buildmosaic.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
+R CMD build mosaicCore
 R CMD build mosaicData
 R CMD build mosaicCalc
 R CMD build mosaicStat
 R CMD build mosaic
+R CMD check mosaicCore
 R CMD check mosaicData
 R CMD check mosaicCalc
 R CMD check mosaicStat
 R CMD check mosaic
-sudo R CMD install mosaicData*gz
-sudo R CMD install mosaicCalc*gz
-sudo R CMD install mosaicStat*gz
 sudo R CMD install mosaic*gz
 mv mosaic*gz Builds
 ls -lrth Builds
