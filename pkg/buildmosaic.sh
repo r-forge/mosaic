c8e5527be58d1a5b6d8526e3a2ca6ba0df624fc6
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-03-21 21:07:13 +0000

    deleting no-longer-needed packages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@58 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/buildmosaic.sh
+++ pkg/buildmosaic.sh
@@ -1,18 +1,6 @@
 #! /bin/bash
 mv mosaic*gz Builds
-R CMD build mosaicCore
-R CMD build mosaicData
-R CMD build mosaicCalc
-R CMD build mosaicStat
 R CMD build mosaic
-R CMD check mosaicCore
-R CMD check mosaicData
-R CMD check mosaicCalc
-R CMD check mosaicStat
 R CMD check mosaic
-sudo R CMD install mosaicCore
-sudo R CMD install mosaicData
-sudo R CMD install mosaicCalc
-sudo R CMD install mosaicStat
 sudo R CMD install mosaic
 ls -lrthd *.tar.gz
--- pkg/mosaicCalc/demo/funplots.R
+++
@@ -1,2 +0,0 @@
-fplot(sin, xlim=c(-2*pi, 2*pi))
-fplot(list(sin,D(sin),D(D(sin)),D(D(D(sin)))), xlim=c(-2*pi, 2*pi))
