0ee9c2be53527f436e6bdf22f1f7e5cb76c1f50c
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-03-24 12:40:13 +0000

    modularized graphical output routine
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaic/R/xqnorm.R
+++ pkg/mosaic/R/xqnorm.R
@@ -1,6 +1,6 @@
 xqnorm <-
 function (p, mean = 0, sd = 1, plot = TRUE, verbose = TRUE, digits = 5, 
-    lower.tail = TRUE, log.p = FALSE, ...) 
+    lower.tail = TRUE, log.p = FALSE, xlim, ylim, ...) 
 {
     q = qnorm(p, mean = mean, sd = sd, lower.tail = lower.tail, 
         log.p = log.p)
@@ -12,35 +12,60 @@ function (p, mean = 0, sd = 1, plot = TRUE, verbose = TRUE, digits = 5,
     }
     if (plot & length(p) == 1) {
         zmax = max(4, abs(z) * 1.6)
-        xlim = mean + c(-1, 1) * zmax * sd
+		if (missing(xlim)) {
+        	xlim = mean + c(-1, 1) * zmax * sd
+		}
         ymax = dnorm(mean, mean = mean, sd = sd)
-        ylim = c(0, 1.3 * ymax)
+		if (missing(ylim)) {
+        	ylim = c(0, 1.4 * ymax)
+		}
         xdata = rep(xlim, each = 2)
         ydata = rep(ylim, times = 2)
         plot <- xyplot(ydata ~ xdata, xlim = xlim, ylim = ylim, 
-            xlab = "", ylab = "density", panel = function(x, 
-                y, ...) {
+            xlab = "", ylab = "density", 
+			panel = function(x, y, ...) {
                 panel.mathdensity(dmath = dnorm, args = list(mean = mean, 
-                  sd = sd), lwd = 2, n = 100, col = "blue")
+                  sd = sd), lwd = 2, n = 100, col = "navy")
+				xs <- seq(xlim[1], q, by=diff(xlim)/500)
+				panel.xyplot(xs, dnorm(xs, mean, sd) , type='h')
                 if (dnorm(q, mean, sd) > 0.5 * ymax) {
                   textloc = c(q, ymax * 0.2)
+                  textloc = c(q, 1.2 * ymax)
                 }
                 else {
                   textloc = c(q, ymax * 0.8)
+                  textloc = c(q, 1.2 * ymax)
                 }
-                panel.segments(q, 0, q, textloc[2] - 0.1 * ymax, 
-                  col = "lightskyblue3")
-                panel.segments(q, textloc[2] + 0.1 * ymax, q, 
-                  ylim[2], col = "lightskyblue3")
-                panel.text(textloc[1], textloc[2], paste(round(q, 
-                  digits)), adj = c(0.5, 0), cex = 1.5)
-                panel.text(textloc[1], textloc[2] - 0.02 * ymax, 
-                  paste("(z=", round(z, 3), ")", sep = ""), adj = c(0.5, 
-                    1), cex = 1.2)
-                panel.text(q, ymax * 1.1, paste(round(p, digits), 
-                  "<-- "), adj = 1, cex = 1.2)
-                panel.text(q, ymax * 1.1, paste(" -->", round(1 - 
-                  p, digits)), adj = 0, cex = 1.2)
+                panel.segments(q, 0, q, unit(ymax,'native') + unit(1.5,'lines'), 
+                  col = "forestgreen", lwd=3)
+                #panel.segments(q, textloc[2] + 0.1 * ymax, q, 
+                #  ylim[2], col = "forestgreen")
+                grid.text(x=q, y=unit(ymax,'native') + unit(2.4,'lines'),  default.units='native',
+					paste(round(q, digits)), 
+					just = c('center','bottom'), gp=gpar(cex = 1.5))
+                grid.text(x=q, y=unit(ymax,'native') + unit(2.4,'lines'), default.units='native',
+                  paste("(z=", round(z, 3), ")", sep = ""), 
+					just = c('center','top'),  gp=gpar(cex = 1.2))
+				grid.lines( gp=gpar(lwd=1.5),
+					x=unit.c( unit(q,'native'), unit(q,'native') - unit(2,'char') ),
+					y=unit(ymax,'native') + unit(.6,'lines'),
+					arrow=arrow(angle=20,length=unit(.75,'char'))
+					)
+                grid.text(
+					x=unit(q,'native') - unit(2,'char'), 
+					y=unit(ymax,'native') + unit(.3,'lines'), default.units='native',
+					paste(round(p, digits), ""), 
+					just = c('right','bottom'),  gp=gpar(cex = 1.2))
+				grid.lines( gp=gpar(lwd=1.5),
+					x=unit.c( unit(q,'native'), unit(q,'native') + unit(2,'char') ),
+					y=unit(ymax,'native') + unit(.6,'lines'),
+					arrow=arrow(angle=20,length=unit(.75,'char'))
+					)
+                grid.text(
+					x=unit(q,'native') + unit(2,'char'), 
+					y=unit(ymax,'native') + unit(.3,'lines'), default.units='native',
+					paste("", round(1 - p, digits)), 
+					just = c('left','bottom'),  gp=gpar(cex = 1.2))
             }, ...)
         print(plot)
     }
