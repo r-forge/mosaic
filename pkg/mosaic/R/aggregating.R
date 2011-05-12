24536dbcb503b41d6492e3e4bf9115cca51d8a40
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-05-12 18:36:31 +0000

    repeater recoded as S4 class and improved; aggregating stats; read.file bug fix for URLs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@298 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaic/R/aggregating.R
+++ pkg/mosaic/R/aggregating.R
@@ -1,8 +1,11 @@
 
 mean <- function(x, ...) UseMethod('mean')
 
-mean.formula <- function( x, data, na.rm=TRUE, ... ) {
-	aggregate( x, data, FUN=base::mean, na.rm=na.rm, ... )
+mean.formula <- function( x, data=parent.frame(), na.rm=TRUE, ... ) {
+	result <- aggregate( x, data, FUN=base::mean, na.rm=na.rm, ... )
+	class(result) <- c('aggregated.stat', class(result))
+	attr(result, 'stat.name') <- 'mean'
+	return(result)
 }
 
 mean.default <- function( x, na.rm=TRUE, ... ) {
@@ -17,7 +20,10 @@ sd.default <- function(x, na.rm=TRUE, ... ) {
 }
 
 sd.formula <- function( x, data, na.rm=TRUE, ... ) {
-	aggregate( x, data, FUN=stats::sd, na.rm=na.rm)
+	result <- aggregate( x, data, FUN=stats::sd, na.rm=na.rm)
+	class(result) <- c('aggregated.stat', class(result))
+	attr(result, 'stat.name') <- 'sd'
+	return(result)
 }
 
 var <- function(x, ...) UseMethod('var')
@@ -27,5 +33,65 @@ var.default <- function(x, na.rm=TRUE, ...) {
 }
 
 var.formula <- function( x, data, na.rm=TRUE, ... ) {
-	aggregate( x, data, FUN=stats::var, na.rm=TRUE)
+	result <- aggregate( x, data, FUN=stats::var, na.rm=na.rm)
+	class(result) <- c('aggregated.stat', class(result))
+	attr(result, 'stat.name') <- 'var'
+	return(result)
+}
+
+median <- function(x, ...) UseMethod('median')
+
+median.default <- function(x, na.rm=TRUE, ...) {
+	stats::median.default(x, na.rm=na.rm)
+}
+
+median.formula <- function( x, data, na.rm=TRUE, ... ) {
+	result <- aggregate( x, data, FUN=stats::median.default, na.rm=na.rm)
+	class(result) <- c('aggregated.stat', class(result))
+	attr(result, 'stat.name') <- 'median'
+	return(result)
+}
+
+count <- function(x, ...) { UseMethod('count') }
+
+count.logical <- function(x, na.rm=TRUE, ...) sum( logical, na.rm=na.rm )
+
+count.factor <- function(x, level=levels(x)[1], na.rm=TRUE, ...) {
+	if (! level %in% levels(x) ) {
+		level = levels(x) [as.numeric(level)]
+	}
+	return( sum( x == level, na.rm=na.rm ) )
+}
+
+count.formula <- function( x, data, na.rm=TRUE, ... ) {
+	result <- aggregate( x, data, FUN=count, na.rm=na.rm)
+	class(result) <- c('aggregated.stat', class(result))
+	attr(result, 'stat.name') <- 'count'
+	return(result)
+}
+
+count.default <- function(x, na.rm=TRUE, ...) {
+	count.factor( as.factor(x), ...)
+}
+
+prop <- function(x, ...) { UseMethod('prop') }
+
+prop.logical <- function(x, na.rm=TRUE, ...) mean( logical, na.rm=na.rm )
+
+prop.factor <- function(x, level=levels(x)[1], na.rm=TRUE, ...) {
+	if (! level %in% levels(x) ) {
+		level = levels(x) [as.numeric(level)]
+	}
+	return( mean( x == level, na.rm=na.rm ) )
+}
+
+prop.formula <- function( x, data, na.rm=TRUE, ... ) {
+	result <- aggregate( x, data, FUN=prop, na.rm=na.rm)
+	class(result) <- c('aggregated.stat', class(result))
+	attr(result, 'stat.name') <- 'prop'
+	return(result)
+}
+
+prop.default <- function(x, na.rm=TRUE, ...) {
+	prop.factor( as.factor(x), ...)
 }
