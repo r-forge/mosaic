8c7955e9584820175c1407b6af323af2deec1720
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-05-14 06:10:58 +0000

    tweaks to core
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@327 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- USCOTS2011/setup.R
+++ USCOTS2011/setup.R
@@ -18,6 +18,16 @@ xyplot <- function(...) { print(lattice::xyplot(...)) }
 bwplot <- function(...) { print(lattice::bwplot(...)) }
 histogram <- function(...) { print(lattice::histogram(...)) }
 xhistogram <- function(...) { print(mosaic::xhistogram(...)) }
+qqmath <- function(...) { print(lattice::qqmath(...)) }
+xqqmath <- function(...) { print(mosaic::xqqmath(...)) }
 dotPlot <- function(...) { print(mosaic::dotPlot(...)) }
 barchart <- function(...) { print(lattice::barchart(...)) }
 densityplot <- function(...) { print(lattice::densityplot(...)) }
+splom <- function(...) { print(lattice::splom(...)) }
+foo <- function(object, correlation = FALSE, symbolic.cor = FALSE, ...) {
+  output <- capture.output( 
+  	stats::summary.lm(object, correlation=correlation, symbolic.cor=symbolic.cor, ...)
+  )
+  prselect(output, stop="Coefficients:")
+}
+
