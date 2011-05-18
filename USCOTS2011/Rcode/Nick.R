df701166a18e9c055e7d6782dda186f2614a7ec4
nhorton (Nicholas Horton) <nicholasjhorton@gmail.com>
2011-05-18 11:39:51 +0000

    Added some routines for twitter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@379 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- USCOTS2011/Rcode/Nick.R
+++ USCOTS2011/Rcode/Nick.R
@@ -1,2 +1,27 @@
+# examples for twitter activity
 
-# put your R code examples here.
+require(twitteR)
+getnumtweets = function(userlist) { 
+  return(sapply(userlist, function(x) x$getStatusesCount()))
+}
+getfollowername = function(userlist) { 
+  return(sapply(userlist, function(x) x$getName()))
+}
+getnumfriends = function(userlist) { 
+  return(sapply(userlist, function(x) x$getFriendsCount()))
+}
+getwhencreated = function(userlist) { 
+  return(sapply(userlist, function(x) x$getCreated()))
+}
+downloadfollowers = function(username) {
+  user = getUser(username) 
+  followers = userFollowers(user)
+  followername = getfollowername(followers)
+  numtweets = getnumtweets(followers)
+  numfriends = getnumfriends(followers)
+  whencreated = getwhencreated(followers)
+  return(data.frame(username=rep(username, length(followers)),
+    followername=followername, numtweets=numtweets,
+    numfriends=numfriends,
+    whencreated=as.POSIXlt(whencreated, origin="1970-01-01")))
+}
