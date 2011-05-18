2e07cfb822c1425b24266c0c3e2e0c46997e98a1
nhorton (Nicholas Horton) <nicholasjhorton@gmail.com>
2011-05-18 13:12:46 +0000

    Updated twitter code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@380 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- USCOTS2011/Rcode/Nick.R
+++ USCOTS2011/Rcode/Nick.R
@@ -1,6 +1,7 @@
 # examples for twitter activity
 
 require(twitteR)
+
 getnumtweets = function(userlist) { 
   return(sapply(userlist, function(x) x$getStatusesCount()))
 }
@@ -13,15 +14,33 @@ getnumfriends = function(userlist) {
 getwhencreated = function(userlist) { 
   return(sapply(userlist, function(x) x$getCreated()))
 }
-downloadfollowers = function(username) {
-  user = getUser(username) 
-  followers = userFollowers(user)
-  followername = getfollowername(followers)
-  numtweets = getnumtweets(followers)
-  numfriends = getnumfriends(followers)
-  whencreated = getwhencreated(followers)
+getscreenname = function(userlist) { 
+  return(sapply(userlist, function(x) x$getScreenName()))
+}
+gettext = function(userlist) { 
+  return(sapply(userlist, function(x) x$getText()))
+}
+downloadfollowers <- function(username) {
+  user <- getUser(username) 
+  followers <- userFollowers(user)
+  followername <- getfollowername(followers)
+  numtweets <- getnumtweets(followers)
+  numfriends <- getnumfriends(followers)
+  whencreated <- getwhencreated(followers)
   return(data.frame(username=rep(username, length(followers)),
     followername=followername, numtweets=numtweets,
     numfriends=numfriends,
     whencreated=as.POSIXlt(whencreated, origin="1970-01-01")))
 }
+
+downloadtweets <- function(searchfield, numtweets=10) {
+  timeline <- searchTwitter(searchfield, n=numtweets)
+  text <- gettext(timeline)
+  screenname <- getscreenname(timeline)
+  whencreated <- getwhencreated(timeline)
+  return(data.frame(username=rep(searchfield, length(whencreated)),
+    screenname=screenname,
+    whencreated=as.POSIXlt(whencreated, origin="1970-01-01"),
+    text=text))
+}
+
