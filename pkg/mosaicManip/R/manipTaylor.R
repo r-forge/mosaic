b152033bb3cafc53d7eb9d1035d1f87bc91a30ee
arich (Mr. ) <andrew.joseph.rich@gmail.com>
2011-06-14 22:49:05 +0000

    More work on showDerivs and manipTaylor, slightly updated documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@417 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaicManip/R/manipTaylor.R
+++ pkg/mosaicManip/R/manipTaylor.R
@@ -3,54 +3,64 @@ manipTaylor = function(expr, xlim = c(-5, 5), ...){
   require("manipulate")
   require("mosaic")
 #functions
-  vals = list(...)
-  .f = mosaic:::.createMathFun( sexpr=substitute(expr), ...)
-  f <<- .f$fun
+   vals = list(...)
+   fm = mosaic:::.createMathFun( sexpr=substitute(expr), ...)
+   f = fm$fun
 x = seq(min(xlim),max(xlim), length = 1000)
+#colors
+trans.blue = rgb(0,0,1,.1) #least squares rectangle color
+line.blue = rgb(0,0,1,.5) #least squares line color
+line.red = rgb(1,0,0,.5) #Taylor line color
 #Derivatives of function for Taylor Series
 dd = list()
-dd[[1]] = D(f~x)
-dd[[2]] = D(f~x+x)
-dd[[3]] = D(f~x+x+x)
-dd[[4]] = D(f~x+x+x+x)
-dd[[5]] = D(f~x+x+x+x+x)
-dd[[6]] = D(f~x+x+x+x+x+x)
-dd[[7]] = D(f~x+x+x+x+x+x+x)
-dd[[8]] = D(f~x+x+x+x+x+x+x+x)
-dd[[9]] = D(f~x+x+x+x+x+x+x+x+x)
-dd[[10]] = D(f~x+x+x+x+x+x+x+x+x+x)
-dd[[11]] = D(f~x+x+x+x+x+x+x+x+x+x+x)
-dd[[12]] = D(f~x+x+x+x+x+x+x+x+x+x+x+x)
+dd[[1]] = mosaic:::.d.symbolic(fm)
+var = fm$RHS
+for(k in 2:10){
+  fm$RHS = c("+", var, fm$RHS)
+dd[[k]] = mosaic:::.d.symbolic(fm)
+}
 
-myplot = function(a, taylor, lsquares){
-  
+myplot = function(a, TaylorBeTrue, lsquares, xwid, n){
+  #Taylor Series
 T = list()
 # zeroth order is index 1
 T[[1]] = f(a) + 0*x
-for (k in 2:13) {
+for (k in 2:11) {
   T[[k]] = T[[k-1]] + (dd[[k-1]](a)*((x-a)^(k-1)))/factorial(k-1)
+}
+
+#Least Squares: Row 1 is order 0
+myx = seq(a-xwid/2, a+xwid/2, length = 10*n+1)
+A = outer(myx, 0:n, "^")
+coefs = qr.solve(A, f(myx))
+lsq.func = coefs[[1]]
+for(j in 2:n)
+{lsq.func = lsq.func+x^(j-1)*coefs[[j]]
 }
 
 #plotting
 mypanel = function(x, y){
   panel.xyplot(x, y, type = "l", col = "black", lwd = 2)
-  panel.points(a, f(a))
-  panel.points(x, T[[as.numeric(taylor)]], type = "l", col = "red", lwd = 2)
+  if(lsquares==TRUE){
+    panel.xyplot(x, lsq.func, type = "l", col = line.blue, lwd = 5)
+    panel.rect(xright=min(myx), xleft=max(myx), ybottom=-999999, ytop = 999999, col = trans.blue) 
+  }
+  panel.points(a, f(a), cex = 2)
+  if(TaylorBeTrue==TRUE){
+  panel.points(x, T[[as.numeric(n)]], type = "l", col = line.red, lwd = 5)
+  }
 }
 x = seq(min(xlim),max(xlim), length = 1000)
 xyplot(f(x)~x, panel = mypanel)
 
 }
 #manipulate that!
-manipulate(myplot(a, taylor, lsquares),
-  a = slider(min(xlim),max(xlim), initial = mean(xlim), step = diff(xlim)/100),
-  taylor = picker(list(T0=1,T1=2, T2=3, T3=4, T4=5, T5=6, T6=7, T7=8, T8=9, T9=10, T10=11, T11=12, T12=13)),
-  lsquares = checkbox(FALSE, "Least Squares")
+manipulate(myplot(a, TaylorBeTrue, lsquares, xwid, n),
+  a = slider(min(xlim),max(xlim), initial = mean(xlim), step = diff(xlim)/100, label = "Position"),
+  TaylorBeTrue = checkbox(FALSE, "Activate Taylor Series"),
+  n = picker(1,2,3,4,5,6,7,8,9,10,11, label = "Order of the Polynomial"),
+  lsquares = checkbox(FALSE, "Activate Least Squares"),
+  xwid = slider(0, diff(range(xlim)), initial = mean(diff(range(xlim)))/2, step = diff(xlim/100), label = "Least Squares width")
   
-  #otherwise
-  #which( taylor == c("T0","T1","T2"
- # taylor = picker("T0","T1","T2","T3","T4","T5","T6","T7","T8", label = "Order of Taylor Approximation"),
-
-  )
-  
+  )  
 }
--- pkg/mosaicManip/R/showDerivs.R
+++ pkg/mosaicManip/R/showDerivs.R
@@ -16,6 +16,7 @@ manipDerivs = function(expr, xlim=c(0,10), ...) {
   integral.color.trans = rgb(0,0,1,.6)
    neg.integral.color2 = "darkslateblue"
   neg.integral.color = rgb(.4,0,1,.6)
+  neg.integral.line.color = rgb(.4,0,1,.3)
   
 #Greater than 0 function
   positive=function(ha){
@@ -54,7 +55,6 @@ myplot= function(xpos, from, der, anti, fixed){
     panel.points(xpos, f(xpos))
     panel.points(from, f(from))
     panel.abline(h=0, lty = "dotted") 
-    #x10 = subset(x, x>(xpos-diff(x)/10)&&x<(xpos+diff(x)/10))
     
     yline = f(xpos)+ dfdx(xpos)*(x - xpos)
     halfwidth=diff(range(x))/10
@@ -63,8 +63,9 @@ myplot= function(xpos, from, der, anti, fixed){
     panel.lines(segEndsX, segEndsY, col=deriv.color2, lwd=10)
     #panel.lines(x, yline, col=deriv.color2, lwd = 10)
     panel.lines(x=c(xpos, -9000000), y = c(f(xpos),f(xpos)), col=rgb(0,0,0,.3), lwd = 11)
-    grid.text(label=paste("Slope = ", signif(dfdx(xpos), 3)), x = unit(1, "npc")-unit(15,"mm"), y = unit(1,"npc")-unit(3,"mm"), gp = gpar(col = deriv.color, fontsize =10))
-  #  rot = atan(dfdx(xpos))*180/pi,
+    #Below was old slope text:
+    #grid.text(label=paste("Slope = ", signif(dfdx(xpos), 3)), x = unit(1, "npc")-unit(15,"mm"), y = unit(1,"npc")-unit(3,"mm"), gp = gpar(col = deriv.color, fontsize =10)) 
+     grid.text(label=paste("Slope = ", signif(dfdx(xpos), 3)), x = unit(xpos+halfwidth/2, "native"), y = unit(f(xpos)+dfdx(xpos)*halfwidth/2,"native"), rot = atan(dfdx(xpos))*180/pi, gp = gpar(col = deriv.color, fontsize =10))
      grid.text(label=round(f(xpos), 3), x = unit(0, "npc")+unit(10,"mm"), y = unit(f(xpos),"native"), gp = gpar(col = "black", fontsize =10))
    }
     
@@ -80,18 +81,23 @@ myplot= function(xpos, from, der, anti, fixed){
     segEndsX = xpos + halfwidth*c(-1,1)
     segEndsY = antiF(xpos, from = from) + f(xpos)*halfwidth*c(-1,1)
     panel.lines(segEndsX, segEndsY, col=rgb(0,0,0,.3), lwd = 10)
+    if(antiF(xpos, from = from)<0)
+    panel.lines(x=c(xpos, -9000000), y = c(antiF(xpos,from = from),antiF(xpos, from=from)), col=neg.integral.line.color, lwd = 11)
+    else
     panel.lines(x=c(xpos, -9000000), y = c(antiF(xpos,from = from),antiF(xpos, from=from)), col=rgb(0,0,1,.3), lwd = 11)
-    grid.text(label=paste("Slope = ", signif(f(xpos), 3)), x = unit(1, "npc")-unit(15,"mm"), y = unit(1,"npc")-unit(3,"mm"), gp = gpar(col = "black", fontsize =10))
-     grid.text(label=round(antiF(xpos), 3), x = unit(0, "npc")+unit(10,"mm"), y = unit(antiF(xpos, from = from),"native"), gp = gpar(col = integral.color, fontsize =10))
+   #Original Slope Text:
+    #grid.text(label=paste("Slope = ", signif(f(xpos), 3)), x = unit(1, "npc")-unit(15,"mm"), y = unit(1,"npc")-unit(3,"mm"), gp = gpar(col = "black", fontsize =10))
+     grid.text(label=paste("Slope = ", signif(f(xpos), 3)), x = unit(xpos+halfwidth/2, "native"), y = unit(antiF(xpos, from=from)+f(xpos)*halfwidth/2,"native"), rot = atan(f(xpos))*180/pi, gp = gpar(col = "black", fontsize =10))
+    grid.text(label=round(antiF(xpos, from = from), 3), x = unit(0, "npc")+unit(10,"mm"), y = unit(antiF(xpos, from = from),"native"), gp = gpar(col = integral.color, fontsize =10))
   }
   
-  grid.newpage()
-  pushViewport(viewport(layout = grid.layout(3,1)))  
-  pushViewport(viewport(layout.pos.row = 2, name = "B"))
-  upViewport()
-  pushViewport(viewport(layout.pos.row = 3, name = "C"))
-  upViewport()
-  pushViewport(viewport(layout.pos.row = 1, name = "A"))
+#   grid.newpage()
+#   pushViewport(viewport(layout = grid.layout(3,1)))  
+#   pushViewport(viewport(layout.pos.row = 2, name = "B"))
+#   upViewport()
+#   pushViewport(viewport(layout.pos.row = 3, name = "C"))
+#   upViewport()
+#   pushViewport(viewport(layout.pos.row = 1, name = "A"))
 
 
 ##create dataframe:
@@ -109,23 +115,25 @@ cc = xyplot(antiF~x, data = dat, type = "l", ylab = "Antiderivative of f", panel
   if(fixed == TRUE)
   { 
     yparam = c(max(antiF(x, from = min(xlim))), min(antiF(x, min(xlim))))
-    diff = diff(yparam)
+    diff = diff(range(yparam))
     yparam = c((min(antiF(x, from = min(xlim))))-diff/3, (max(antiF(x, from = min(xlim))))+diff/3)
     cc = xyplot(antiF~x, data = dat, type = "l", ylim = yparam, ylab = "Antiderivative of f", panel = antiFpanel)  
   }
 
  if(der == TRUE){
-print(a, newpage = FALSE)
-
+# print(a, newpage = FALSE)
+print(a, position = c(0, 0.65, 1, 1), more = TRUE)
 }
-upViewport(2)
+# upViewport(2)
  
-downViewport("B")
-print(b, newpage = FALSE)
-upViewport()
+# downViewport("B")
+# print(b, newpage = FALSE)
+print(b, position = c(0, 0.34, 1, 0.69), more = TRUE)
+# upViewport()
 if(anti == TRUE){
- downViewport("C")
-print(cc, newpage = FALSE)
+#  downViewport("C")
+# print(cc, newpage = FALSE)
+print(cc, position = c(0,0,1,0.40))
 }
 }
  #Making the plot
