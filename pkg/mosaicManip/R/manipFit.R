0f439a3162eb59c4792553c26407eb947d2e0da4
arich (Mr. ) <andrew.joseph.rich@gmail.com>
2011-06-21 18:23:37 +0000

    debugging mFit with Prof. Kaplan
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@435 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaicManip/R/manipFit.R
+++ pkg/mosaicManip/R/manipFit.R
@@ -1,38 +1,48 @@
 mFit = function(data, expr=NULL, ...){
 
+  f1 = function(x) rep.int(1, length(x))
+  f2 = function(x) x
+  f3 = function(x) x^2
+  f4 = function(x) x^3
+  f5 = function(x) log(x)
+  f6 = function(x, k) exp(k*x)
+  f7 = function(x, P) sin(2*pi*x/P)
+  f8 = function(x, P) cos(2*pi*x/P)
+  f9 = function(x, mu, sd) pnorm(q = x, mean = mu, sd = sd)
   
-   
-  myPlot = function(xvar, yvar, k, n, P, mu, sad){
+ #####  
+  myPlot = function(xvar, yvar, k, n, P, mu, sd){
    xvals = data[[xvar]]
    yvals = data[[yvar]]
 
    x = seq(min(xvals),max(xvals), length = 1000)
+  
    A = matrix(nrow=length(xvals),ncol = 9)
-   A[,1] = rep.int(1,length(x))
-   A[,2] = x
-   A[,3] = x^2
-   A[,4] = x^3
-   A[,5] = log(x)
-   A[,6] = exp(k*x)
-   A[,7] = sin(2*pi*x/P)
-   A[,8] = cos(2*pi*x/P)
-   A[,9] = pnorm(q = x, mean = mu, sd = 5)
-   
+   A[,1] = f1(xvals)
+   A[,2] = f2(xvals)
+   A[,3] = f3(xvals)
+   A[,4] = f4(xvals)
+   A[,5] = f5(xvals)
+   A[,6] = f6(x=xvals, k=k)
+   A[,7] = f7(x=xvals, P=P)
+   A[,8] = f8(x=xvals, P=P)
+   A[,9] = f9(x=xvals, mu=mu, sd=sd)
+   browser()   
    coefs = qr.solve(A, yvals)
-   browser()
+
    
   #PLOTTING F'REAL
      xyplot(xvals~yvals, data, xlab = xvar, ylab = yvar)
 
   }
- 
- manipulate(myPlot(xvar, yvar, k, P, mu, sad),
+ #####
+ manipulate(myPlot(xvar=xvar, yvar=yvar, k=k, n=n, P=P, mu=mu, sd=sd),
             xvar = picker(as.list(names(data)), label = "X Variable"),
             yvar = picker(as.list(names(data)), label = "Y Variable"),
             k=slider(-10,10, step = .1),
             P=slider(-10,10, step = .1),
-            mu = slider(-100,100, step =.1),
-            sad = slider(1, 20, step = .1, initial = 3)
+            mu = slider(-100,100, step =.1, initial = 50),
+            sd = slider(1, 20, step = .1, initial = 3)
             )
 }
-#min(data[[xvar]],max(data[[xvar]])
+#
