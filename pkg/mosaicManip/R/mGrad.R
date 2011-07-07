c93104b33595d1228efd0357b4d09c6af476f58a
arich (Mr. ) <andrew.joseph.rich@gmail.com>
2011-07-07 18:59:48 +0000

    Lunch, mGrad fixed besides the perpendicular issue
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@490 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaicManip/R/mGrad.R
+++ pkg/mosaicManip/R/mGrad.R
@@ -22,6 +22,18 @@ mGrad = function(expr, ..., xlim = c(0,10), ylim = c(0,10)){
  funy$RHS = funy$RHS[3]
  dy = mosaic:::.doD(funy, ..h..=NULL, numerical=FALSE, method="center",...)
  
+ get.aspect.ratio = function() {
+    y = convertY( unit(1,"native"),unitTo= "cm", valueOnly=TRUE)
+    x = convertX( unit(1,"native"),unitTo= "cm",valueOnly=TRUE) 
+#    y = convertUnit( unit(1,"native"),"cm",typeFrom="dimension", axisFrom="y",axisTo="y",valueOnly=TRUE)
+#    x = convertUnit( unit(1,"native"),"cm",typeFrom="dimension", axisFrom="x",axisTo="x",valueOnly=TRUE)
+   print(x)
+   print(y)
+   print(y/x)
+   return(y/x)
+   
+}
+ 
 #=============================== 
   myFun = function(npts=npts, narrows = narrows, scale=scale, nlevels = nlevels, 
                    components=components){
@@ -32,19 +44,11 @@ mGrad = function(expr, ..., xlim = c(0,10), ylim = c(0,10)){
     c.xpt = mean(xlim2)  #center point and vectors
     c.ypt = mean(ylim2)
     
-    arrow.grid=matrix(ncol=2)                      #create a grid of evenly spaced arrows
-    xwid=diff(range(xlim2))/(narrows+1)
-    ywid=diff(range(ylim2))/(narrows+1)
-    xpts = seq(min(xlim2),max(xlim2), length = narrows)
-    ypts = seq(min(ylim2),max(ylim2), length = narrows)
-    
-#     for(j in 1:narrows){
-#       for(k in 1:narrows){
-#         arrow.grid=rbind(arrow.grid, c(k*xwid, j*ywid))
-#       }
-#     }
-#     arrow.grid=arrow.grid[-1,]
-   # xpts = c(arrow.grid[,1]); ypts = c(arrow.grid[,2])  #Functions wouldn't take a list, so concatenate to a vector
+    xpts = seq(min(xlim2),max(xlim2), length = narrows+1)  #Create grid of evenly spaced points
+    xpts = rep(xpts, times = narrows+1)
+    ypts = seq(min(ylim2),max(ylim2), length = narrows+1)
+    ypts = rep(ypts, each = narrows+1)
+
     xpts=c(xpts,as.numeric(c.xpt)) 
     ypts=c(ypts,as.numeric(c.ypt))
     xvecs = dx(x=xpts, y=ypts)    #Derivatives for the gradient
@@ -54,17 +58,18 @@ mGrad = function(expr, ..., xlim = c(0,10), ylim = c(0,10)){
     
     bigstart = .05; bigend = .95  #rainbow colors are clearer when it goes from one extreme to the other, not red to red
     mylevels = pretty(range(.zset),nlevels)   #number of contours
-    image( .xset, .yset, .zset, col=rainbow(npts, alpha=0.8, start=bigstart, end=bigend),
+    levelplot( .xset, .yset, .zset, col=rainbow(npts, alpha=0.8, start=bigstart, end=bigend),
              add=FALSE, xlab=xlab,ylab=ylab,main=NULL )
-    contour(.xset, .yset, .zset, col=rgb(0,0,0,.5),lwd=3,add=TRUE, labcex=1.2, 
+    contourplot(.xset, .yset, .zset, col=rgb(0,0,0,.5),lwd=3,add=TRUE, labcex=1.2, 
               levels=mylevels, method="edge")
            
     arrows(x0=xpts, y0=ypts, x1=xpts+xvecs, y1=ypts+yvecs, lwd = 3)
   #  arrows(x0=c.xpt, y0=c.ypt, x1=c.xpt+c.xvec, y1=c.ypt+c.yvec, lwd=3)
-           browser()
+    get.aspect.ratio()
     if(components){
-      close = which(xpts>(max(xlim2)/4)&xpts<(3*max(xlim2)/4)
-                    &ypts>(max(xlim2)/4)&ypts<(3*max(xlim2)/4))
+      close = which(xpts>(diff(range(xlim2))/4+min(xlim2))&xpts<(3*diff(range(xlim2))/4+min(xlim2))
+                    &(ypts>(diff(range(ylim2))/4+min(ylim2)))&(ypts<3*(diff(range(ylim2))/4)+min(ylim2)))
+      
       close.xpts=xpts[close]
       close.ypts=ypts[close]
       close.xvecs=xvecs[close]
@@ -76,8 +81,8 @@ mGrad = function(expr, ..., xlim = c(0,10), ylim = c(0,10)){
 #=====================================      
  manipulate(myFun(npts=npts, narrows = narrows, scale=scale, nlevels = nlevels, 
                   components=components),
-            npts = slider(20,200,initial=100,label = "Number of pixels"),
-            narrows = slider(0,8, initial = 2, step = 1, label = "Number of arrows per row"),
+            npts = slider(20,200,initial=140,label = "Number of pixels"),
+            narrows = slider(0,10, initial = 5, step = 1, label = "Number of arrows per row"),
             scale = slider(.01, 1, step = .01, initial = .5, label="% Arrow scale"),
             nlevels = slider(5, 50, initial = 20, step = 1, label = "Approx. number of contour lines"),
             components = checkbox(FALSE, label = "Display components of gradient")
@@ -85,5 +90,6 @@ mGrad = function(expr, ..., xlim = c(0,10), ylim = c(0,10)){
 }
             
 #Make one central arrow DONE
-#for 5 or 9 around it x and y components    
-#Make sure arrows can go in negative space    
+#for 5 or 9 around it x and y components    DONE
+#Make sure arrows can go in negative space    DONE
+#Fix aspect ratio so arrows are always perpendicular
