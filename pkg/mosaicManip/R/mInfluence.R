34b45268c91c03011deb24b0ab56bcc3a6107dea
arich (Mr. ) <andrew.joseph.rich@gmail.com>
2011-07-20 19:59:08 +0000

    Lunch, mInfluence updated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@529 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaicManip/R/mInfluence.R
+++ pkg/mosaicManip/R/mInfluence.R
@@ -10,19 +10,39 @@ mInfluence=function(expr, data){
     #temp=as.character(terms[a])
     xvar[[a]]=a
   }
+  maxsd=0
+  for(b in 1:(nterms-1)){
+    maxsd=max(maxsd, sd(data[[xvar[[b]]]]), na.rm=TRUE) #HERE categorical variables return In var(as.vector(x), na.rm = na.rm) : NAs introduced by coercion
+  }
+
   #====================
-  myFun=function(xpick){
-    mod=lm(expr, data)
+  myFun=function(xpick, whereX, whereY, newPoint){    
+    newRow=data.frame()
+    colnames(newRow)=colnames(data)
+browser()
+    newRow[[1,xvar[[xpick]]]]=whereX
+    newRow[[1,yvar]]=whereY
+    modData=rbind(data, newRow)
+    browser()
+    if(newPoint)
+      data=modData
     
+    mod=lm(expr, data)
+    coefs=coef(mod)
     influ=influence.measures(mod)
     influpts=which(apply(influ$is.inf, 1, any))
     influpts=data[influpts,]
-    browser()
+
     myPanel=function(x,y,...){
       panel.xyplot(x,y, ...)
       lpoints(influpts[[xvar[[xpick]]]], influpts[[yvar]], col="red")
-      panel.abline(mod)  
+      panel.abline(mod)
+      if(newPoint){
+        lpoints(modData[dim(modData)[1],], col="orange", cex=2)
+        print(modData[dim(modData)[1],])
+      }
     }
+    
 #     if(is.factor(data[[yvar]]))
 #       data[[yvar]]=jitter(data[[yvar]])
 
@@ -31,8 +51,13 @@ mInfluence=function(expr, data){
   }
   #==================
   controls=list(xpick=picker(xvar, label="Dep. Variable to plot"),
+                newPoint=checkbox(FALSE, label="Add new point"),
+                whereX=slider(-5*maxsd, 5*maxsd, 
+                              initial=1*maxsd, label="New x"),
+                whereY=slider(-5*maxsd, 5*maxsd, 
+                              initial=1*maxsd, label="New y"),
                 influPick=picker(cooks.distance="Cook's Distance", label="Type of Influence")
                 )
-  manipulate(myFun(xpick=xpick), 
+  manipulate(myFun(xpick=xpick, whereX=whereX, whereY=whereY, newPoint=newPoint), 
              controls)
 }
