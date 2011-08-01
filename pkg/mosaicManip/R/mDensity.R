0aa4ba7d11991b40e56778bb964769dbdcbd834c
arich (Mr. ) <andrew.joseph.rich@gmail.com>
2011-08-01 18:25:27 +0000

    Added mDensity and mDist
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@553 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaicManip/R/mDensity.R
+++ pkg/mosaicManip/R/mDensity.R
@@ -15,19 +15,17 @@ mDensity=function(data=NULL){
     trans.x=trans(x)
     densityplot(trans.x, xlab= "Values", bw=bandwidth,
                 scales = list(x=list(cex=1.5)))
-
   }
   #===================
   controls=list(bandwidth=slider(.01, 5, initial=1, step=.01, label="Bandwidth"),
-             dist=picker("Normal Dist."=1, "Uniform"=2, "Exponential"=3, label="What function?"),
-             trans=picker("None"=c, "Log"=log, "Sqrt"=sqrt, "Rank"=rank, "arcCos"=acos)
-             )
-              
+                trans=picker("None"=c, "Log"=log, "Sqrt"=sqrt, "Rank"=rank, "arcCos"=acos, label="Transformation")
+             )     
     if(is.null(data)){
+      controls[["dist"]]=picker("Normal Dist."=1, "Uniform"=2, "Exponential"=3, label="What function?")
       controls[["seed"]]=slider(1, 100, step=1, label="Random seed", initial=1)
       controls[["npts"]]=slider(10,1000, step=1, initial=100, label="Number of points")
     }
-  
+  #=====================
   manipulate(myFun(bandwidth=bandwidth, dist=dist, trans=trans, seed=seed, npts=npts),
              controls)
 }
--- pkg/mosaicManip/R/mHist.R
+++ pkg/mosaicManip/R/mHist.R
@@ -5,22 +5,22 @@ mHist=function(data=NULL){
   .iter=0
   if(.iter==0)
     oldBreaks<<-NULL
-  if(is.null(data)){
-    x=rnorm(500, mean=0, sd=5)
-  }
-  else
-    x=data
+  
   #===============
   myFun=function(nBands, cBreak, seed){
   if(is.null(data))  set.seed(seed)
     .iter<<-.iter+1
-    
+  if(is.null(data)){
+    x=rnorm(500, mean=0, sd=5)
+    }
+  else
+    x=data
     
     rng=diff(range(x))
     myby=rng/(nBands)
     c.rng = diff(c(min(x),cBreak))
-    minratio= c.rng/rng
-    maxratio= 1-minratio
+#     minratio= c.rng/rng
+#     maxratio= 1-minratio      #I believe these lines are not necessary. 
     myBreaks = seq(cBreak, (max(x)+rng/5), by=myby)
     myBreaks = c(seq(cBreak, (min(x)-rng/5), by=-myby), myBreaks)
     
@@ -34,15 +34,12 @@ mHist=function(data=NULL){
     newHist=histogram(x, panel=myPan, type = "density",
                       scales = list(x=list(cex=1.7)))
     print(newHist)
-       
-    oldBreaks<<-myBreaks
+    oldBreaks<<-myBreaks   #Keep the old breaks for next time
   }
-  
-  
-       controls = list(nBands=slider(1, 50, step=1, initial=10, label="Number of histogram bins"),
-             cBreak=slider(mean(x)-3,mean(x)+3, step=.01, initial=mean(x), label="Center Break")
+       controls = list(nBands=slider(4, 50, step=1, initial=10, label="Number of histogram bins"),
+             cBreak=slider(mean(x)-3,mean(x)+3, step=.01, initial=signif(mean(x), 3), label="Center Break")
              )
-      if(is.null(data))
+      if(is.null(data))   #seed slider only shows up when data is null
         controls$seed=slider(1,100,step=1, initial=1, label="Random seed")
 
  #========================
