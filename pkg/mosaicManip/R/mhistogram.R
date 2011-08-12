4a5cf3e8e23b614d910c11212fbb107702b2c1ee
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-08-12 02:04:17 +0000

    bug fixes in mhistogram(), xhistogram()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@567 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mosaicManip/R/mhistogram.R
+++ pkg/mosaicManip/R/mhistogram.R
@@ -2,11 +2,19 @@
 mhistogram <- function(..., n=12, nint=n) {
 	if ( require(manipulate) ) {
 		manipulate(
-			histogram( ..., nint = N ),
-			N = slider( 3, 50, initial=nint )
+			xhistogram( ..., nint = N, fit=FIT ),
+			N = slider( 3, 50, initial=nint ),
+			FIT = picker( none=NULL, 
+						 normal="normal", 
+						 "log-normal" = "log-normal",
+						 exponential='exponential', 
+						 gamma='gamma'
+						 )
 		)
 	}
 	else {
 		histogram(..., nint=nint)
 	}
 }
+
+
