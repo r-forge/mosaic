4016bdf55f85a30187ee6271ebe68763048def47
rpruim (Randall Pruim) <rpruim@calvin.edu>
2011-12-21 20:25:43 +0000

    added export _R_CHECK_FORCE_SUGGESTS_=false and --resave-data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mosaic@594 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/buildmosaic.sh
+++ pkg/buildmosaic.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+export _R_CHECK_FORCE_SUGGESTS_=false
 mv mosaic*gz Builds
-R CMD build mosaic
+R CMD build --resave-data mosaic
 R CMD check mosaic
 sudo R CMD install mosaic
 ls -lrthd *.tar.gz
--- pkg/buildmosaicManip.sh
+++ pkg/buildmosaicManip.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mv mosaicManip*gz Builds
-R CMD build mosaicManip
+R CMD build --resave-data mosaicManip
 R CMD check mosaicManip
 sudo R CMD install mosaicManip
 ls -lrthd *.tar.gz
